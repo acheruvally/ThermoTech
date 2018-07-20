0dbf79e7d905bf1aa6e7e4d7a037382dc319fb5e
Ajith Cheruvally <acheruvally@gmail.com>
2018-07-20 10:26:18 +0530

    Working Code with Mock Data

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const express = require('express');
 var app = express();
 const FlowByTDSRange = require('./MockData/FlowByTDSRange.json');
-const FlowByTempRange =  require('./MockData/FlowByTempRange.json');
-const DayWiseUsage =  require('./MockData/DayWiseUsage.json');
+const FlowByTempRange = require('./MockData/FlowByTempRange.json');
+const DayWiseUsage = require('./MockData/DayWiseUsage.json');
 const bodyparser = require('body-parser');
-const FlowByHour =  require('./MockData/FlowByHour.json');
+const FlowByHour = require('./MockData/FlowByHour.json');
 const MySQLCredentials = require('./MySQLCredentials.json');
 
 
@@ -48,167 +48,194 @@ app.get('/SolarHeaterData', (req, res) => {
 //Get Flow by Temperature Range
 app.get('/FlowByTempRange', (req, res) => {
 
-    var inDate = req.query.date;
-    if (inDate) {
-        fromDate = inDate + 'T00:00:00.000Z'
-        toDate = inDate + 'T23:59:59.000Z';
-    }
-    else {
-        fromDate = '2018-07-07T00:00:00.000Z';
-        toDate = '2018-07-07T23:59:59.000Z';
-    }
+    // Mock Data for testing
+    res.send(FlowByTempRange);
 
-    var query = "with FlowCount as ( SELECT FlowMeter , Temperature , case when  Temperature  between 0 and  20 then '0-20'" +
-        " when  Temperature  between 20 and 40 then '20-40'" +
-        " when  Temperature  between 40 and 60 then '40-60'" +
-        " when  Temperature  > 60 then '>60'" +
-        " end as 'TempRange' FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
-        " select TempRange , sum(FlowMeter) as Count from FlowCount group by TempRange";
+    // ---------------------------------------
+    //  Commented for testing with mock data
+    // ---------------------------------------
+    // var inDate = req.query.date;
+    // if (inDate) {
+    //     fromDate = inDate + 'T00:00:00.000Z'
+    //     toDate = inDate + 'T23:59:59.000Z';
+    // }
+    // else {
+    //     fromDate = '2018-07-07T00:00:00.000Z';
+    //     toDate = '2018-07-07T23:59:59.000Z';
+    // }
 
-
-    mysqlConnection.query(query, (err, rows, fields) => {
-        var output = { Temp: undefined };
-        if (!err) {
-            output.Temp = rows;
-            res.send(output);
-
-        }
+    // var query = "with FlowCount as ( SELECT FlowMeter , Temperature , case when  Temperature  between 0 and  20 then '0-20'" +
+    //     " when  Temperature  between 20 and 40 then '20-40'" +
+    //     " when  Temperature  between 40 and 60 then '40-60'" +
+    //     " when  Temperature  > 60 then '>60'" +
+    //     " end as 'TempRange' FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
+    //     " select TempRange , sum(FlowMeter) as Count from FlowCount group by TempRange";
 
 
-        else
-            console.log(err);
-    })
+    // mysqlConnection.query(query, (err, rows, fields) => {
+    //     var output = { Temp: undefined };
+    //     if (!err) {
+    //         output.Temp = rows;
+    //         res.send(output);
+
+    //     }
+
+
+    //     else
+    //         console.log(err);
+    // })
 });
 
 //Get Flow by TDS Range
 app.get('/FlowByTDSRange', (req, res) => {
 
-    var inDate = req.query.date;
-    if (inDate) {
-        fromDate = inDate + "T00:00:00.000Z"
-        toDate = inDate + "T23:59:59.000Z";
-    }
-    else {
-        fromDate = "2018-07-07T00:00:00.000Z";
-        toDate = "2018-07-07T23:59:59.000Z";
-    }
 
-    var query = "with FlowCount as ( SELECT FlowMeter , TDS , case when  TDS  between 0 and  150 then '0-150'" +
-        " when  TDS  between 150 and 300 then '150-300'" +
-        " when  TDS  between 300 and 450 then '300-450'" +
-        " when  TDS  > 450 then '>450'" +
-        " end as 'TDSRange' FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
-        " select TDSRange , sum(FlowMeter) as Flow from FlowCount group by TDSRange";
+    // Mock Data for testing
+    res.send(FlowByTDSRange);
+
+    // ---------------------------------------
+    //  Commented for testing with mock data
+    // ---------------------------------------
+
+    // var inDate = req.query.date;
+    // if (inDate) {
+    //     fromDate = inDate + "T00:00:00.000Z"
+    //     toDate = inDate + "T23:59:59.000Z";
+    // }
+    // else {
+    //     fromDate = "2018-07-07T00:00:00.000Z";
+    //     toDate = "2018-07-07T23:59:59.000Z";
+    // }
+
+    // var query = "with FlowCount as ( SELECT FlowMeter , TDS , case when  TDS  between 0 and  150 then '0-150'" +
+    //     " when  TDS  between 150 and 300 then '150-300'" +
+    //     " when  TDS  between 300 and 450 then '300-450'" +
+    //     " when  TDS  > 450 then '>450'" +
+    //     " end as 'TDSRange' FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
+    //     " select TDSRange , sum(FlowMeter) as Flow from FlowCount group by TDSRange";
 
 
-    mysqlConnection.query(query, (err, rows, fields) => {
-        var output = { TDS: undefined };
-        if (!err) {
-            output.TDS = rows;
-            res.send(output);
-            
-        }
+    // mysqlConnection.query(query, (err, rows, fields) => {
+    //     var output = { TDS: undefined };
+    //     if (!err) {
+    //         output.TDS = rows;
+    //         res.send(output);
+
+    //     }
 
 
-        else
-            console.log(err);
-    })
+    //     else
+    //         console.log(err);
+    // })
 });
 
 //Get Daywise Usage
 app.get('/DaywiseUsage', (req, res) => {
 
+    // Mock Data for testing
+    res.send(DayWiseUsage);
 
-    var fromDate = req.query.fromDate;
-    var toDate = req.query.toDate;
+    // ---------------------------------------
+    //  Commented for testing with mock data
+    // ---------------------------------------
 
-    if (!fromDate) {
+    // var fromDate = req.query.fromDate;
+    // var toDate = req.query.toDate;
 
-        fromDate = '2018-07-07T00:00:00.000Z';
+    // if (!fromDate) {
 
-    }
-    if (!toDate) {
-        toDate = '2018-07-07T23:59:59.000Z';
-    }
+    //     fromDate = '2018-07-07T00:00:00.000Z';
 
-
-
-    var query = "select sum(FlowMeter) as activeHours, CAST(Date(Measuredat) AS char) as date FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" +
-        " group by Date(Measuredat) order by Date(Measuredat) asc";
-
-    mysqlConnection.query(query, (err, rows, fields) => {
-        var output = { Usage: undefined };
-        if (!err) {
-            output.Usage = rows;
-            res.send(output);
-
-        }
+    // }
+    // if (!toDate) {
+    //     toDate = '2018-07-07T23:59:59.000Z';
+    // }
 
 
-        else
-            console.log(err);
-    })
+
+    // var query = "select sum(FlowMeter) as activeHours, CAST(Date(Measuredat) AS char) as date FROM thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" +
+    //     " group by Date(Measuredat) order by Date(Measuredat) asc";
+
+    // mysqlConnection.query(query, (err, rows, fields) => {
+    //     var output = { Usage: undefined };
+    //     if (!err) {
+    //         output.Usage = rows;
+    //         res.send(output);
+
+    //     }
+
+
+    //     else
+    //         console.log(err);
+    // })
 });
 
 //Get Hourly Flow Data 
 app.get('/FlowByHour', (req, res) => {
 
-
-    var inDate = req.query.date;
-    if (inDate) {
-        fromDate = inDate + 'T00:00:00.000Z'
-        toDate = inDate + 'T23:59:59.000Z';
-    }
-    else {
-        fromDate = '2018-07-07T00:00:00.000Z';
-        toDate = '2018-07-07T23:59:59.000Z';
-    }
+    // Mock Data for testing
+    res.send(FlowByHour);
 
 
-    var query = " with flow as ( select FlowMeter , Hour(MeasuredAt) as MeasuredHour, case when  Temperature  between 0 and  20 then '0-20'" +
-        " when  Temperature  between 20 and 40 then '20-40' " +
-        " when  Temperature  between 40 and 60 then '40-60' " +
-        " when  Temperature  > 60 then '>60'  end as 'TempRange' from thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
-        " select sum(FlowMeter) as Flow, MeasuredHour , Temprange from flow group by MeasuredHour , Temprange";
+    // ---------------------------------------
+    //  Commented for testing with mock data
+    // ---------------------------------------
+
+    // var inDate = req.query.date;
+    // if (inDate) {
+    //     fromDate = inDate + 'T00:00:00.000Z'
+    //     toDate = inDate + 'T23:59:59.000Z';
+    // }
+    // else {
+    //     fromDate = '2018-07-07T00:00:00.000Z';
+    //     toDate = '2018-07-07T23:59:59.000Z';
+    // }
 
 
-    mysqlConnection.query(query, (err, rows, fields) => {
-        var output = {"HourlyFlowData":[]};
-        if (!err) {
+    // var query = " with flow as ( select FlowMeter , Hour(MeasuredAt) as MeasuredHour, case when  Temperature  between 0 and  20 then '0-20'" +
+    //     " when  Temperature  between 20 and 40 then '20-40' " +
+    //     " when  Temperature  between 40 and 60 then '40-60' " +
+    //     " when  Temperature  > 60 then '>60'  end as 'TempRange' from thermotech.solarheaterdata where MeasuredAt between " + "'" + fromDate + "'" + " and " + "'" + toDate + "'" + " )" +
+    //     " select sum(FlowMeter) as Flow, MeasuredHour , Temprange from flow group by MeasuredHour , Temprange";
 
 
-            for (var i = 0; i < 24; i++) {
-
-                var match = rows.find(function (obj) { return obj.MeasuredHour === i; });
-                var Hour = new Date(fromDate.substring(0,10));
-                Hour.setUTCHours(i);
-
-                if (match) {
-
-                    switch (match.TempRange)
-                    {
-                        case "0-20"  : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow1": match.Flow , "Flow2" : 0 ,"Flow3" : 0 ,"Flow4" : 0  }); break; 
-                        case "20-40" : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow2": match.Flow , "Flow1" : 0 ,"Flow3" : 0 ,"Flow2" : 0  }); break;
-                        case "40-60" : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow3": match.Flow , "Flow1" : 0 ,"Flow3" : 0 ,"Flow2" : 0  }); break;
-                        case ">60"   : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow4": match.Flow , "Flow2" : 0 ,"Flow3" : 0 ,"Flow1" : 0  }); break;
-                    }
-
-                }
-
-                else {
-                    output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow1": 0 ,"Flow4": 0 , "Flow2" : 0 ,"Flow3" : 0 });
-                }
-
-            }
-
-            res.send(output);
-
-        }
+    // mysqlConnection.query(query, (err, rows, fields) => {
+    //     var output = {"HourlyFlowData":[]};
+    //     if (!err) {
 
 
-        else
-            console.log(err);
-    })
+    //         for (var i = 0; i < 24; i++) {
+
+    //             var match = rows.find(function (obj) { return obj.MeasuredHour === i; });
+    //             var Hour = new Date(fromDate.substring(0,10));
+    //             Hour.setUTCHours(i);
+
+    //             if (match) {
+
+    //                 switch (match.TempRange)
+    //                 {
+    //                     case "0-20"  : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow1": match.Flow , "Flow2" : 0 ,"Flow3" : 0 ,"Flow4" : 0  }); break; 
+    //                     case "20-40" : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow2": match.Flow , "Flow1" : 0 ,"Flow3" : 0 ,"Flow2" : 0  }); break;
+    //                     case "40-60" : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow3": match.Flow , "Flow1" : 0 ,"Flow3" : 0 ,"Flow2" : 0  }); break;
+    //                     case ">60"   : output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow4": match.Flow , "Flow2" : 0 ,"Flow3" : 0 ,"Flow1" : 0  }); break;
+    //                 }
+
+    //             }
+
+    //             else {
+    //                 output.HourlyFlowData.push({ "Hour": Hour.toJSON(), "Flow1": 0 ,"Flow4": 0 , "Flow2" : 0 ,"Flow3" : 0 });
+    //             }
+
+    //         }
+
+    //         res.send(output);
+
+    //     }
+
+
+    //     else
+    //         console.log(err);
+    // })
 });
 
 
@@ -277,7 +304,7 @@ app.post('/SolarHeaterData', (req, res) => {
     let SolarHeaterData = req.body;
     var sql = "SET @SensorID = ?;SET @MeasuredAt = ?;SET @FlowMeter = ?;SET @Temperature = ?;SET @TDS = ?; \
     CALL AddSolarHeaterData(@SensorID,@MeasuredAt,@FlowMeter,@Temperature,@TDS);";
-    mysqlConnection.query(sql, [SolarHeaterData.SensorID, SolarHeaterData.MeasuredAt, SolarHeaterData.FlowMeter, SolarHeaterData.Temperature,SolarHeaterData.TDS], (err, rows, fields) => {
+    mysqlConnection.query(sql, [SolarHeaterData.SensorID, SolarHeaterData.MeasuredAt, SolarHeaterData.FlowMeter, SolarHeaterData.Temperature, SolarHeaterData.TDS], (err, rows, fields) => {
         if (!err)
 
             res.send(JSON.stringify('Success'));
